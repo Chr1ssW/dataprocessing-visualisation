ea243015d3abc5d84c8dfe1b6723f80ce81479c8
Chr1ssW <56091550+Chr1ssW@users.noreply.github.com>
2021-01-14 14:55:46 +0100

    Chart added with real data

--- index.php
+++ index.php
@@ -1,14 +1,6 @@
 <!DOCTYPE html>
 <html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="style.css">
-    <title>Visualisation</title>
-</head>
-<body>
-
-    <?php 
+<?php 
     error_reporting(E_ERROR | E_PARSE);
     $jsonAmazon = file_get_contents("http://localhost:8090/amazonTitles/format=json");
     $jsonArrayAmazon = json_decode($jsonAmazon, true);
@@ -27,7 +19,7 @@
     {
         $key = $jsonArrayAmazon[$i]['yearOfRelease'];
 
-        if ($key != 0)
+        if ($key != 0 && $key > 2010)
         {
             $amazonTitlesPerYear[$key] = $amazonTitlesPerYear[$key] + 1;
         }
@@ -38,7 +30,7 @@
         $key = $jsonArrayDisneyPlus[$i]['addedAt'];
         $key = (int)substr($key, -4);
 
-        if ($key != 0)
+        if ($key != 0 && $key > 2010)
         {
             $disneyPlusTitlesPerYear[$key] = $disneyPlusTitlesPerYear[$key] + 1;
         }
@@ -49,16 +41,93 @@
         $key = $jsonArrayNetflix[$i]['dateAdded'];
         $key = (int)substr($key, -4);
 
-        if ($key != 0)
+        if ($key != 0 && $key > 2010)
         {
             $netflixTitlesPerYear[$key] = $netflixTitlesPerYear[$key] + 1;
         }
     }
 
-    echo "<pre>";
-    var_dump($netflixTitlesPerYear);
-    echo "</pre>";
+    $merged = array();
+
+     
+
+    foreach ($amazonTitlesPerYear as $key => $value)
+    {
+        $merged[$key][0] = $value;
+    }
+
+    foreach ($disneyPlusTitlesPerYear as $key => $value)
+    {
+        $merged[$key][1] = $value;
+    }
+
+    foreach ($netflixTitlesPerYear as $key => $value)
+    {
+        $merged[$key][2] = $value;
+    }
+
+    ksort($merged);
     ?>
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <title>Visualisation</title>
+    <script type="text/javascript" src="https://www.gstatic.com/charts/loader.js"></script>
+    <script type="text/javascript">
+      google.charts.load('current', {'packages':['corechart']});
+      google.charts.setOnLoadCallback(drawChart);
+
+      function drawChart() {
+        var data = google.visualization.arrayToDataTable([
+          ['Year', 'Amazon', 'Disney+', 'Netflix'],
+          <?php
+            foreach ($merged as $key => $value)
+            {
+                $amazon = 0;
+                $disneyPlus = 0;
+                $netflix = 0;
+
+                if ($value[0] != null)
+                {
+                    $amazon = $value[0];
+                }
+
+                if ($value[1] != null)
+                {
+                    $disneyPlus = $value[1];
+                }
+
+                if ($value[2] != null)
+                {
+                    $netflix = $value[2];
+                }
+
+
+                echo "[' $key ', $amazon , $disneyPlus ,  $netflix ],";
+            }
+          ?>
+        ]);
+
+        var options = {
+          title: 'Number of titles of big streaming services from 2011',
+          legend: { position: 'bottom' },
+          series: {
+              0: {color: '#212e3e'},
+              1: {color: '#1e3e8e'},
+              2: {color: '#de0912'}
+          }
+        };
+
+        var chart = new google.visualization.LineChart(document.getElementById('curve_chart'));
+
+        chart.draw(data, options);
+      }
+    </script>
+</head>
+<body>
+
     
+    <div id="curve_chart" style="width: 900px; height: 500px"></div>
 </body>
 </html>
