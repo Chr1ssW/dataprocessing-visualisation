681b53dff52cb02d15c12a943d8422d2fbc9e402
Chr1ssW <56091550+Chr1ssW@users.noreply.github.com>
2021-01-14 15:24:01 +0100

    Added column chart
    Column chart shows all titles per streaming service

--- index.php
+++ index.php
@@ -1,7 +1,10 @@
 <!DOCTYPE html>
 <html lang="en">
 <?php 
+    //Hiding warnings because PHP is bugging out -> not a good practice but no better solution right now
     error_reporting(E_ERROR | E_PARSE);
+
+    //Loading values form the datasest
     $jsonAmazon = file_get_contents("http://localhost:8090/amazonTitles/format=json");
     $jsonArrayAmazon = json_decode($jsonAmazon, true);
 
@@ -15,6 +18,9 @@
     $disneyPlusTitlesPerYear = array();
     $netflixTitlesPerYear = array();
 
+
+    //Loading how many titles each service added in each year from 2010
+    //Amazon
     for ($i = 0; $i < sizeof($jsonArrayAmazon); $i++)
     {
         $key = $jsonArrayAmazon[$i]['yearOfRelease'];
@@ -25,6 +31,7 @@
         }
     }
 
+    //Disney+
     for ($i = 0; $i < sizeof($jsonArrayDisneyPlus); $i++)
     {
         $key = $jsonArrayDisneyPlus[$i]['addedAt'];
@@ -36,6 +43,7 @@
         }
     }
 
+    //Netflix
     for ($i = 0; $i < sizeof($jsonArrayNetflix); $i++)
     {
         $key = $jsonArrayNetflix[$i]['dateAdded'];
@@ -47,9 +55,8 @@
         }
     }
 
+    //Combining the three datasets to one assoc. array
     $merged = array();
-
-     
 
     foreach ($amazonTitlesPerYear as $key => $value)
     {
@@ -66,7 +73,16 @@
         $merged[$key][2] = $value;
     }
 
+    //Sorting the array based on key
     ksort($merged);
+
+
+    //ALl titles per streaming service
+    $amazonAllTitles = sizeof($jsonArrayAmazon);
+    $disneyPlusAllTitles = sizeof($jsonArrayDisneyPlus);
+    $netflixAllTitles = sizeof($jsonArrayNetflix);
+
+
     ?>
 <head>
     <meta charset="UTF-8">
@@ -119,15 +135,46 @@
           }
         };
 
-        var chart = new google.visualization.LineChart(document.getElementById('curve_chart'));
+        var chart = new google.visualization.AreaChart(document.getElementById('curve_chart'));
 
         chart.draw(data, options);
       }
     </script>
+    <script type="text/javascript">
+        google.charts.load("current", {packages:['corechart']});
+        google.charts.setOnLoadCallback(drawChart);
+
+        function drawChart() {
+        var data = google.visualization.arrayToDataTable([
+            ["Service", "Titles", { role: "style" } ],
+            ["Amazon", <?php echo $amazonAllTitles?>, "#212e3e"],
+            ["Disney+", <?php echo $disneyPlusAllTitles?>, "#1e3e8e"],
+            ["Netflix", <?php echo $netflixAllTitles?>, "#de0912"]
+        ]);
+
+        var view = new google.visualization.DataView(data);
+        view.setColumns([0, 1,
+                        { calc: "stringify",
+                            sourceColumn: 1,
+                            type: "string",
+                            role: "annotation" },
+                        2]);
+
+        var options = {
+            title: "Titles per streaming service",
+            width: 600,
+            height: 400,
+            bar: {groupWidth: "95%"},
+            legend: { position: "none" },
+        };
+        var chart = new google.visualization.ColumnChart(document.getElementById("columnchart_values"));
+        chart.draw(view, options);
+        }
+    </script>
+
 </head>
 <body>
-
-    
     <div id="curve_chart" style="width: 900px; height: 500px"></div>
+    <div id="columnchart_values" style="width: 900px; height: 300px;"></div>
 </body>
 </html>
